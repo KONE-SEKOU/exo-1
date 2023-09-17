d0a7e6985e9d6c925b66b5874add4f719092ee73
kone sekou <kone15061@icloud.com>
2023-09-17 11:31:03 +0000

    exo 1

---
+++ exo1.php
@@ -0,0 +1,11 @@
+<?php 
+
+    class Personne{
+
+            public $homme;
+            public $femme;
+
+            public function genre(){
+                echo " quelle est votre genre";
+            }
+    }
